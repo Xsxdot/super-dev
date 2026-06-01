946abe7c2353e47ac91748b126bc35fbeb8717cf
xsx <xsx@shanzilai.com>
2026-06-01 18:48:47 +0800

    Remove Swift SuperDev app and MCP

--- SuperDev/SuperDev/AppCore/Models/RunSummary.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct RunSummary: Identifiable, Equatable {
-    var id: UUID { runId }
-    let runId: UUID
-    let startTime: Date
-    let logCount: Int
-    let serviceNames: [String]
-}
